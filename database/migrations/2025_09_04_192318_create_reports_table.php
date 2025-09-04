4f3f92444fe7a52104c3e0d45cc1ec0de2bca151
Hector Oswaldo Lopez Maza <oswal.369@hotmail.com>
2025-09-04 15:29:23 -0600

    Creacion de model, m y f
    Falta modificar los modelos y las ultimas tablas de migracion

---
+++ database/migrations/2025_09_04_192318_create_reports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre_empresa');      
+            $table->string('giro_empresa');
+            $table->integer('telefono');
+            $table->string('representante');
+            $table->date('fecha_analisis');
+            $table->foreignId('user_id')->constrained('users'); //Creada
+            $table->string('colaborador1');
+            $table->string('colaborador2');
+            $table->string('colaborador3');
+            $table->string('colaborador4');
+            $table->string('colaborador5');
+            $table->string('logo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
---
+++ database/migrations/2025_09_04_192909_create_report_titles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report_titles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('report_id')->constrained('report'); //Creada
+            $table->foreignId('title_id')->constrained('titles'); //Creada
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report_titles');
+    }
+};
