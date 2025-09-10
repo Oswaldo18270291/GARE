5a834717f1a34f95a129c2af6e790492e7cc0949
ItzelGaldamez <galdamez15itzel29@gmail.com>
2025-09-09 19:36:48 -0800

    Fondo del inicio de sesión
    Se agrego diseño en el inicio de sesión, y se hizo el cambio de letra en el titulo "GARE"

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,9 +1,11 @@
 @props([
     'title',
     'description',
+    'titleClass' => '', // clase opcional para el título
+    'descClass' => '',  // clase opcional para la descripción
 ])
 
 <div class="flex w-full flex-col text-center">
-    <flux:heading size="xl">{{ $title }}</flux:heading>
-    <flux:subheading>{{ $description }}</flux:subheading>
+    <flux:heading size="xl" class="{{ $titleClass }}">{{ $title }}</flux:heading>
+    <flux:subheading class="{{ $descClass }}">{{ $description }}</flux:subheading>
 </div>
