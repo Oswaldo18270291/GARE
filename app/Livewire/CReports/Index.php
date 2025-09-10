7a67e06dcfe195f9b9765fcaf5c02a8f9283a2a2
Hector Oswaldo Lopez Maza <oswal.369@hotmail.com>
2025-09-09 19:56:42 -0600

    Despliegue de contenido con check box

--- app/Livewire/CReports/Index.php
+++ app/Livewire/CReports/Index.php
@@ -17,37 +17,6 @@ class Index extends Component
         $this->titles = Title::with('subtitles.sections')->get();
     }
 
-     public function save()
-    {
-        foreach ($this->selectedTitles as $titleId) {
-            $reportTitle = ReportTitle::create([
-                'report_id' => $this->reportId,
-                'title_id' => $titleId,
-                'status' => true,
-            ]);
-
-            $subtitles = $this->selectedSubtitles[$titleId] ?? [];
-            foreach ($subtitles as $subtitleId) {
-                $reportSubtitle = ReportTitleSubtitle::create([
-                    'r_t_id' => $reportTitle->id,
-                    'subtitle_id' => $subtitleId,
-                    'status' => true,
-                ]);
-
-                $sections = $this->selectedSections[$subtitleId] ?? [];
-                foreach ($sections as $sectionId) {
-                    ReportTitleSubtitleSection::create([
-                        'r_t_s_id' => $reportSubtitle->id,
-                        'section_id' => $sectionId,
-                        'status' => true,
-                    ]);
-                }
-            }
-        }
-
-        session()->flash('success', 'Guardado correctamente.');
-        $this->reset(['selectedTitles', 'selectedSubtitles', 'selectedSections']);
-    }
 
     public function render()
     {
