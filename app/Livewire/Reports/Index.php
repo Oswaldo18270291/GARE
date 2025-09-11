ab138615e304a884fa3013dac327e912bfab6d81
Hector Oswaldo Lopez Maza <oswal.369@hotmail.com>
2025-09-10 19:54:52 -0600

    Mis informes se modifico
    Se muestra ya los informes creados

--- app/Livewire/Reports/Index.php
+++ app/Livewire/Reports/Index.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Livewire\Reports;
-
+use Livewire\WithPagination;
+use App\Models\Report;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.reports.index');
+        return view('livewire.reports.index',[
+            'reports'=> Report::paginate(10)
+        ]);
     }
+
 }
