50e1d7b5587c9933c1369e6bfc71c1e11f75fca8
Hector Oswaldo Lopez Maza <oswal.369@hotmail.com>
2025-09-15 12:18:23 -0600

    Div de la definición
    Se cambio los parámetros de la definición para que no se pierda el contenido

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,17 +2,17 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         
         <div class="grid auto-rows-min gap-4 md:grid-cols-2">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <div class="relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 aspect-video md:aspect-auto">
                 <x-placeholder-pattern class="absolute inset-0 size-full dark:stroke-neutral-100/20" />
                 <img src="inicio/definicion.png">
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border" style="border-color:rgba(31, 89, 177, 1);">
+            <div class="relative overflow-hidden rounded-xl border p-4" style="border-color:rgba(31, 89, 177, 1);">
                 <x-placeholder-pattern class="absolute inset-0 size-full dark:stroke-neutral-100/20" />
-                <p style="font-size: 14px; color: black; position: absolute; margin: 5px; top: 5%; left: 5%; right: 5%;">
+                <p class="text-sm text-black relative z-10">
                     GARE (Gestión de Análisis de Riesgos Evaluados) es un sistema diseñado para la Secretaría de Seguridad Pública, cuyo propósito es facilitar la captura, organización y generación de informes de análisis de riesgos.
                     <br><br>
                     Este sistema permite a los usuarios:</p> 
-                    <ul style="list-style-type: disc; padding-left: 10%; margin-top: 130px; font-size: 14px;">
+                    <ul class="list-disc pl-6 mt-4 text-sm relative z-10 space-y-2">
                         <li>Registrar y estructurar de manera ordenada la información requerida.</li>
                         <li>Generar automáticamente un archivo PDF con el formato y la estructura oficial correspondiente.</li>
                         <li>Optimizar el tiempo de elaboración de informes, garantizando uniformidad, precisión y respaldo documental.</li>
