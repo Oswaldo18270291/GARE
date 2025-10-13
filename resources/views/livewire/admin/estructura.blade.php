96188dd4b02cdbe33e0ae45d7346b0f24aac5965
ItzelGaldamez <galdamez15itzel29@gmail.com>
2025-10-12 20:34:00 -0800

    Cambios a nivel interfaz
    Se agrego los comentarios de de actualizar "titulo, subtitulo y sección"
    Se agrego el fondo gris de la tabla usuarios y se cambio a español la tabla

--- resources/views/livewire/admin/estructura.blade.php
+++ resources/views/livewire/admin/estructura.blade.php
@@ -25,6 +25,7 @@
                             class="border rounded px-2 py-1 w-2/3">
                         <button wire:click="saveTitle({{ $t->id }})" class="px-3 py-1 bg-blue-700 text-white rounded">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <title>Actualizar título</title>
                                 <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
                             </svg>
                         </button>
@@ -49,6 +50,7 @@
                                             class="border rounded px-2 py-1 w-2/3">
                                         <button wire:click="saveSubtitle({{ $st->id }})" class="px-3 py-1 bg-blue-500 text-white rounded">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                                <title>Actualizar subtítulo</title>
                                                 <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
                                             </svg>
                                         </button>
@@ -72,6 +74,7 @@
                                                         class="border rounded px-2 py-1 w-2/3">
                                                     <button wire:click="saveSection({{ $sec->id }})" class="px-3 py-1 bg-blue-300 text-white rounded">
                                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                                            <title>Actualizar sección</title>
                                                             <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
                                                         </svg>
                                                     </button>
@@ -81,6 +84,7 @@
                                     </ul>
                                 </li>
                             @endforeach
+                            
                         </div>
                     </ul>
                 </li>
